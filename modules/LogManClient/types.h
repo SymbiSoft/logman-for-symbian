de99370be7fca4a20a398c812284cc48f4220b0c
jtoivola <jtoivola@localhost>
2008-10-28 18:41:56 +0000

    Added 'get' command for printing out file contents.

--- modules/LogManClient/types.h
+++ modules/LogManClient/types.h
@@ -45,8 +45,8 @@ cog.outl( data % ( vermaj, vermin, build, svnrev ) )
 
 const TUint KLogServMajorVersionNumber=1;
 const TUint KLogServMinorVersionNumber=8;
-const TUint KLogServBuildVersionNumber=1026;
-const TUint KRevisionNumber=26;
+const TUint KLogServBuildVersionNumber=1028;
+const TUint KRevisionNumber=29;
 
 ///[[[end]]]
 

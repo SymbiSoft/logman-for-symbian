e5f47e54bc8d4374040b1cbb84335651ea56ff7c
jtoivola <jtoivola@localhost>
2008-11-12 10:59:03 +0000

    Applied fixes from branches/shell and contributed patch to fix issues: 1. Problem with LogManClient::Log - Log("%S", des) and Log(des) results in different output 2. logmanutils.h - should include "LogMan.h" and problems with _INFO macros - Changed to #include <logman/logman.h> 3. install_headers.py - missing types.h

--- modules/LogManClient/logmanutils.h
+++ modules/LogManClient/logmanutils.h
@@ -2,7 +2,7 @@
 #define LOGMANUTILS_H
 
 #if defined( __LOGMAN_ENABLED__ )
-    #include <LogMan.h>
+    #include <logman/LogMan.h>
 
     #pragma message( RLogMan logging enabled )
 
@@ -15,7 +15,20 @@
         #define __LOGMAN_MSG_PREFIX__ __FILE__ ":" __LOGMAN_TOSTR(__LINE__) "> "
     #endif
 
-    #define GETMSG(msg) _L8( __LOGMAN_MSG_PREFIX__ msg "\n" )
+    // These are for internal use only, to get parameters expanded correctly
+    #define __LOGMAN_GETMSG( msg ) _L( msg )
+    #define __LOGMAN_GETMSG8( msg ) _L8( msg )
+    #define __LOGMAN_GETMSG16( msg ) _L16( msg )
+
+    // Get message, defined in different sizes if needed
+    // Be aware that TDes16 and TDes8 should not be combined when Formatting
+    #define LOGMAN_GETMSG(msg) __LOGMAN_GETMSG( __LOGMAN_MSG_PREFIX__ msg "\n" )
+    #define LOGMAN_GETMSG8(msg) __LOGMAN_GETMSG8( __LOGMAN_MSG_PREFIX__ msg "\n" )
+    #define LOGMAN_GETMSG16(msg) __LOGMAN_GETMSG16( __LOGMAN_MSG_PREFIX__ msg "\n" )
+
+    // just for backward compatibility
+    #define GETMSG(msg) LOGMAN_GETMSG8(msg)
+
 
     // Use 'MACRO __LOGMAN_SEND_ASYNC__' in mmp to enable asynchronous sending
     #ifdef __LOGMAN_SEND_ASYNC__
@@ -35,13 +48,17 @@
     // Using static RLogMan functions( a bit slower than using member instance )
     // =========================================================================================
     // Easy one line logging
-    #define LOGMAN_SENDLOG( x )  RLogMan::Log( GETMSG(x), __LOGMAN_SEND_ASYNC__ );
+    #define LOGMAN_SENDLOG( x )  RLogMan::Log( LOGMAN_GETMSG8(x), __LOGMAN_SEND_ASYNC__ );
     // Easy one line logging, specify async mode explicitly
-    #define LOGMAN_SENDLOG2( x, async )  RLogMan::Log( GETMSG(x) ), async );
+    #define LOGMAN_SENDLOG2( x, async )  RLogMan::Log( LOGMAN_GETMSG8(x), async );
     // Easy one line logging with formatting
-    #define LOGMAN_SENDLOGF( x, args... )  RLogMan::Log( GETMSG(x), __LOGMAN_SEND_ASYNC__, args );
+    #define LOGMAN_SENDLOGF( x, args... )  RLogMan::Log( LOGMAN_GETMSG(x), __LOGMAN_SEND_ASYNC__, args );
+    #define LOGMAN_SENDLOGF8( x, args... )  RLogMan::Log( LOGMAN_GETMSG8(x), __LOGMAN_SEND_ASYNC__, args );
+    #define LOGMAN_SENDLOGF16( x, args... )  RLogMan::Log( LOGMAN_GETMSG16(x), __LOGMAN_SEND_ASYNC__, args );
     // Easy one line logging with formatting, specify async mode explicitly
-    #define LOGMAN_SENDLOGF2( x, async, args... )  RLogMan::Log( GETMSG(x), async, args );
+    #define LOGMAN_SENDLOGF2( x, async, args... )  RLogMan::Log( LOGMAN_GETMSG(x), async, args );
+    #define LOGMAN_SENDLOGF28( x, async, args... )  RLogMan::Log( LOGMAN_GETMSG(x), async, args );
+    #define LOGMAN_SENDLOGF216( x, async, args... )  RLogMan::Log( LOGMAN_GETMSG(x), async, args );
     // Log line of code before executing it.
     #define LOGMAN_SENDLINE( line ) LOGMAN_SENDLOG( #line ); line
     // Write descriptor directly
@@ -49,7 +66,7 @@
     // Easy one liner for logging information about thread's stack usage
     #define LOGMAN_SEND_STACK_INFO() { RLogMan __logman;__logman.Connect();__logman.StackInfo(); __logman.Close(); }
     // Easy one liner for logging information about thread's heap usage
-    #define LOGMAN_SEND_STACK_INFO() { RLogMan __logman;__logman.Connect();__logman.HeapInfo(); __logman.Close(); }
+    #define LOGMAN_SEND_HEAP_INFO() { RLogMan __logman;__logman.Connect();__logman.HeapInfo(); __logman.Close(); }
     // Easy one liner for logging information about thread's memory usage
     #define LOGMAN_SEND_MEMORY_INFO() { RLogMan __logman;__logman.Connect();__logman.MemoryInfo(); __logman.Close(); }
         
@@ -64,12 +81,16 @@
     #define LOGMAN_INIT     LOGMAN_MEMBER_VARIABLE = new (ELeave) RLogMan; LOGMAN_MEMBER_VARIABLE->Connect();
 
     // Send log
-    #define LOGMAN_LOG( x )         LOGMAN_MEMBER_VARIABLE->Write( GETMSG(x), __LOGMAN_SEND_ASYNC__ );
-    #define LOGMAN_LOG2( x, async ) LOGMAN_MEMBER_VARIABLE->Write( GETMSG(x), async );
+    #define LOGMAN_LOG( x )         LOGMAN_MEMBER_VARIABLE->Write( LOGMAN_GETMSG8(x), __LOGMAN_SEND_ASYNC__ );
+    #define LOGMAN_LOG2( x, async ) LOGMAN_MEMBER_VARIABLE->Write( LOGMAN_GETMSG8(x), async );
 
     // Send log with formatting
-    #define LOGMAN_LOGF( x, args... )         LOGMAN_MEMBER_VARIABLE->Writef( GETMSG(x) , __LOGMAN_SEND_ASYNC__, args );
-    #define LOGMAN_LOGF2( x, async, args... ) LOGMAN_MEMBER_VARIABLE->Writef( GETMSG(x) , async, args );
+    #define LOGMAN_LOGF( x, args... )         LOGMAN_MEMBER_VARIABLE->Writef( LOGMAN_GETMSG(x) , __LOGMAN_SEND_ASYNC__, args );
+    #define LOGMAN_LOGF8( x, args... )         LOGMAN_MEMBER_VARIABLE->Writef( LOGMAN_GETMSG8(x) , __LOGMAN_SEND_ASYNC__, args );
+    #define LOGMAN_LOGF16( x, args... )         LOGMAN_MEMBER_VARIABLE->Writef( LOGMAN_GETMSG16(x) , __LOGMAN_SEND_ASYNC__, args );
+    #define LOGMAN_LOGF2( x, async, args... ) LOGMAN_MEMBER_VARIABLE->Writef( LOGMAN_GETMSG(x) , async, args );
+    #define LOGMAN_LOGF28( x, async, args... ) LOGMAN_MEMBER_VARIABLE->Writef( LOGMAN_GETMSG8(x) , async, args );
+    #define LOGMAN_LOGF216( x, async, args... ) LOGMAN_MEMBER_VARIABLE->Writef( LOGMAN_GETMSG16(x) , async, args );
 
     #define LOGMAN_STACK_INFO()  LOGMAN_MEMBER_VARIABLE->StackInfo();
     #define LOGMAN_HEAP_INFO()   LOGMAN_MEMBER_VARIABLE->HeapInfo();
@@ -87,17 +108,27 @@
     #define LOGMAN_SENDLOG(  x, args... )
     #define LOGMAN_SENDLOG2( x, async, args...)
     #define LOGMAN_SENDLOGF( x, args... )
+    #define LOGMAN_SENDLOGF8( x, args... )
+    #define LOGMAN_SENDLOGF16( x, args... )
     #define LOGMAN_SENDLOGF2( x, async, args... )
+    #define LOGMAN_SENDLOGF28( x, async, args... )
+    #define LOGMAN_SENDLOGF216( x, async, args... )
     #define LOGMAN_SENDLINE( line ) line
     #define LOGMAN_SENDDESC( desc )
     #define LOGMAN_SEND_STACK_INFO()
+    #define LOGMAN_SEND_HEAP_INFO()
+    #define LOGMAN_SEND_MEMORY_INFO()
     
     #define LOGMAN_MEMBER
     #define LOGMAN_INIT
     #define LOGMAN_LOG( x )
     #define LOGMAN_LOG2( x, async )
     #define LOGMAN_LOGF( x, args... )
+    #define LOGMAN_LOGF8( x, args... )
+    #define LOGMAN_LOGF16( x, args... )
     #define LOGMAN_LOGF2( x, async, args... )
+    #define LOGMAN_LOGF28( x, async, args... )
+    #define LOGMAN_LOGF216( x, async, args... )
     #define LOGMAN_STACK_INFO() 
     #define LOGMAN_HEAP_INFO()  
     #define LOGMAN_MEMORY_INFO()
